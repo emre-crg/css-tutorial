935a02cc9753951ec980108b3313d79df3092e4b
emre crg <vrtemre@gmail.com>
2020-09-17 17:53:24 +0300

    GulpJs tasks have been edited.

--- booking-site/gulpfile.js
+++ booking-site/gulpfile.js
@@ -1,39 +1,3 @@
-// //Old Version
-// const gulp = require('gulp')
-// const browserSync = require('browser-sync').create()
-// const sass = require('gulp-sass')
-// const nodemon = require('gulp-nodemon')
-// const prefix = require('gulp-autoprefixer')
-// const sourcemaps = require('gulp-sourcemaps')
-// const reload = browserSync.reload
-
-// gulp.task('browser-sync', ['start'], function () {
-//   browserSync.init({
-//     server: {
-//       baseDir: './'
-//     }
-//   })
-//   gulp.watch('./*.html').on('change', reload)
-//   gulp.watch('./scss/**/*.scss', ['css'])
-// })
-
-// gulp.task('start', function () {
-//   return nodemon({
-//     script: 'server.js'
-//   })
-// })
-
-// gulp.task('css', () => {
-//   return gulp.src('./scss/main.scss')
-//   .pipe(sass())
-//   .pipe(prefix())
-//   .pipe(gulp.dest('./'))
-//   .pipe(browserSync.stream())
-// })
-
-// gulp.task('default', ['browser-sync', 'html', 'css'])
-
-//New Version:
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
@@ -76,7 +40,7 @@ gulp.task(
   gulp.series('nodemon', () => {
     browserSync.init(null, {
       proxy: 'http://localhost:3000',
-      files: ['public/**/*.*'],
+      files: ['./**/*.html','./sass/**/*.scss','./*.js'],
       port: 5000
     });
   })
