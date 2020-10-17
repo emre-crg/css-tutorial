90fe0f6051aebaeabd3b51023e7d117c1142d725
emre crg <vrtemre@gmail.com>
2020-10-17 05:27:20 +0300

    header is complete

---
+++ booking-site/js/main.js
@@ -0,0 +1,8 @@
+$(".Header-switch").on("click", function(){
+    if($(".Header").is(".menu-show")) {
+        $(".Header").removeClass("menu-show")
+    }
+    else {
+        $(".Header").addClass("menu-show")
+    }
+})
